b0f69b695c33c3c7eed00169a87719e9f001e53e
aleenaneo <aleena.george@neopraxisin>
2024-08-17 11:14:27 +0530

    3red commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Age from './components/Age';
+import Image from './components/Image';
 
 function App() {
   return (
     <div className="App">
       <h1>hiiiii</h1>
+      <Age />
+      <Image/>
     </div>
   );
 }
