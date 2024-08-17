b2d3d74cb339e5992a363acef85e532cbf14bdc3
aleenaneo <aleena.george@neopraxisin>
2024-08-17 12:38:53 +0530

    Feat: name with blue color added.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
 
       <h1 style={{color:"blue"}}>hii aleenaaaaaaaaa</h1>
 
+=======
+      <h1 style={{color:"blue"}}>hii aleenaaaaaaaaa</h1>
+>>>>>>> fd86903 (Feat: name with blue color added.)
     </div>
   );
 }
