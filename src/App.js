a147629242b7a9083ae0cf8816270a541a5fff2b
aleenaneo <aleena.george@neopraxisin>
2024-08-19 10:54:01 +0530

    committ Conflict Resolve

--- src/App.js
+++ src/App.js
@@ -3,13 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-
-      <h1 style={{color:"blue"}}>hii aleenaaaaaaaaa</h1>
-
-=======
-      <h1 style={{color:"blue"}}>hii aleenaaaaaaaaa</h1>
->>>>>>> fd86903 (Feat: name with blue color added.)
+            <h1 style={{color:"blue"}}>hii aleenaaaaaaaaa</h1>
     </div>
   );
 }
